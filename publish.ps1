1ae9795078d576358ec014dffd586e6326bfbc59
Kevin <campi121@umn.edu>
2026-04-01 10:28:58 -0500

    update

--- publish.ps1
+++ publish.ps1
@@ -2,7 +2,7 @@ $start_dir = $PSScriptRoot
 
 Set-Location 'C:\Users\kcamp\Downloads\baseball\src'
 
-#python -m analytics.all --all_players 'True' --modules Relievers #for late night runs
+python -m analytics.all #--all_players 'True' --injuries "Y" #for late night runs
 #python -m analytics.all --all_players "True" --program 2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025
 python -m dashboard.visualization --module Current --year 2026 #generate then publish
 
@@ -12,4 +12,6 @@ git add .
 git commit -m 'update'
 git push
 
-#Set-Location 'C:\Users\kcamp\Downloads\baseball\src'
+Set-Location 'C:\Users\kcamp\Downloads\baseball\src'
+
+python -m analytics.all --all_players "True" --program 2019,2020,2021,2022,2023,2024,2025
