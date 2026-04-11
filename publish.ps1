6f3a2a57d4ecbdc5d796b873970cd345b846a671
Kevin <campi121@umn.edu>
2026-04-11 18:57:06 -0500

    update

--- publish.ps1
+++ publish.ps1
@@ -5,7 +5,8 @@ Set-Location 'C:\Users\kcamp\Downloads\baseball\src'
 #python -m analytics.all #--all_players 'True' --injuries "Y" #for late night runs
 #python -m analytics.all --all_players "True" --program 2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025
 python -m utilities.team_assignment 2015-2026
-python -m dashboard.dashboard_matchups --min_pa 25 --min_ip 5
+#python -m dashboard.dashboard_matchups --min_pa 25 --min_ip 5
+python -m dashboard.dashboard_matchups --year 2025 --min_pa 75 --min_ip 15 --rosters --rosters_year 2025
 python -m dashboard.visualization --module Current --year 2026 #generate then publish
 
 Set-Location $start_dir
